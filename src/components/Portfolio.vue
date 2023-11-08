b2330b7512da9010ab4212f6e297e41711b5a1cb
alisastoten <alisastoten7@gmail.com>
2023-11-08 22:27:01 +0200

    services changed

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -1,17 +1,7 @@
 
 <template>
   <div class="min-h-screen text-white text-center">
-    <div class="flex">
-      <div class="w-3/12 h-screen-md custom-gradient m-3 py-5 rounded-2xl">
-        <h1 class="text-4xl text-xl text-yellow-200 mb-2">{{ msg }}</h1>
-        <p>
-          More text coming soon.
-        </p>
-      </div>
-      <div class="w-9/12 bg-pink-900 m-3 py-5 rounded-2xl">
-        <p>Some other text here</p>
-      </div>
-    </div>
+    
   </div>
   
 
--- src/components/Services.vue
+++ src/components/Services.vue
@@ -5,10 +5,10 @@
       <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12">
         <div v-for="service in services" :key="service.name" class="rounded rounded-3xl services-bg">
           <h2 class="p-4 mb-5 text-xl text-black font-extrabold rounded rounded-3xl custom-yellow-bg">{{ service.name }}</h2>
-          <ul>
-            <li v-for="point in service.description" :key="point" class="text-left px-7 mb-5">{{ point }}</li>
+          <ul class="list-disc">
+            <li v-for="point in service.description" :key="point" class="text-left text-md mx-12 mb-5">{{ point }}</li>
           </ul>
-          <p class="font-semibold text-xl pt-2 my-3">{{ service.price }}</p>
+          <p class="font-semibold text-2xl pt-2 my-4">{{ service.price }}</p>
         </div>
       </div>
     </div>
