7f5f6333795dd0ba3419f6ae9ee2bbdd1c18ea0b
alisastoten <alisastoten7@gmail.com>
2023-11-12 16:31:10 +0200

    Porfolio pictures for veebipoed added

---
+++ src/components/Job2.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="w-6/8 mx-auto text-white mt-10 flex flex-col justify-center">
+    <div class="flex flex-col">
+      <h2 class="text-2xl sm:text-3xl font-semibold mt-4">Eesmärk</h2>
+      <p class="mt-4 leading-relaxed">Luua B2B reklaamid, et Veebipoed.ee jõuaks rohkemate klientideni.</p>
+      <h2 class="text-2xl sm:text-3xl font-semibold mt-10 mb-5">Reklaamid</h2>
+      <p class="mb-5">Karusell reklaam 1</p>
+      <div class="grid grid-cols-1 md:grid-cols-4 gap-2 justify-items-center">
+        <img src="../assets/tehtud-tood/veebipoed/carousel1-1-400.jpg" alt="" class="rounded-xl">
+        <img src="../assets/tehtud-tood/veebipoed/carousel1-2-400.jpg" alt="" class="rounded-xl">
+        <img src="../assets/tehtud-tood/veebipoed/carousel1-3-400.jpg" alt="" class="rounded-xl">
+      </div>
+      <p class="mb-5 mt-10">Karusell reklaam 2</p>
+      <div class="grid grid-cols-1 md:grid-cols-4 gap-2 justify-items-center">
+        <img src="../assets/tehtud-tood/veebipoed/carousel6-1-400.jpg" alt="" class="rounded-xl">
+        <img src="../assets/tehtud-tood/veebipoed/carousel6-2-400.jpg" alt="" class="rounded-xl">
+        <img src="../assets/tehtud-tood/veebipoed/carousel6-3-400.jpg" alt="" class="rounded-xl">
+      </div>
+      <p class="mb-5 mt-10">Karusell reklaam 3</p>
+      <div class="grid grid-cols-1 md:grid-cols-5 gap-2">
+        <img src="../assets/tehtud-tood/veebipoed/carousel2-1-400.jpg" alt="" class="rounded-xl">
+        <img src="../assets/tehtud-tood/veebipoed/carousel2-2-400.jpg" alt="" class="rounded-xl">
+        <img src="../assets/tehtud-tood/veebipoed/carousel2-3-400.jpg" alt="" class="rounded-xl">
+        <img src="../assets/tehtud-tood/veebipoed/carousel2-4-400.jpg" alt="" class="rounded-xl">
+        <img src="../assets/tehtud-tood/veebipoed/carousel2-5-400.jpg" alt="" class="rounded-xl">
+      </div>
+      <p class="mb-5 mt-10">Karusell reklaam 4</p>
+      <div class="grid grid-cols-1 md:grid-cols-6 gap-2">
+        <img src="../assets/tehtud-tood/veebipoed/carousel3-1-400.jpg" alt="" class="rounded-xl">
+        <img src="../assets/tehtud-tood/veebipoed/carousel3-2-400.jpg" alt="" class="rounded-xl">
+        <img src="../assets/tehtud-tood/veebipoed/carousel3-3-400.jpg" alt="" class="rounded-xl">
+        <img src="../assets/tehtud-tood/veebipoed/carousel3-4-400.jpg" alt="" class="rounded-xl">
+        <img src="../assets/tehtud-tood/veebipoed/carousel3-5-400.jpg" alt="" class="rounded-xl">
+        <img src="../assets/tehtud-tood/veebipoed/carousel3-6-400.jpg" alt="" class="rounded-xl">
+      </div>
+      <p class="mb-5 mt-10">Karusell reklaam 5</p>
+      <div class="grid grid-cols-1 md:grid-cols-6 gap-2">
+        <img src="../assets/tehtud-tood/veebipoed/carousel5-1-400.jpg" alt="" class="rounded-xl">
+        <img src="../assets/tehtud-tood/veebipoed/carousel5-2-400.jpg" alt="" class="rounded-xl">
+        <img src="../assets/tehtud-tood/veebipoed/carousel5-3-400.jpg" alt="" class="rounded-xl">
+        <img src="../assets/tehtud-tood/veebipoed/carousel5-4-400.jpg" alt="" class="rounded-xl">
+        <img src="../assets/tehtud-tood/veebipoed/carousel5-5-400.jpg" alt="" class="rounded-xl">
+      </div>
+
+    </div>
+  </div>  
+</template>
--- src/components/PortfolioDetails.vue
+++ src/components/PortfolioDetails.vue
@@ -1,17 +1,17 @@
 <template>
     <div class="flex flex-col md:flex-row items-center justify-center mt-10">
-      <img :src="job.img" alt="Job Image" class="w-5/6 md:w-2/10 rounded-lg" />
-      <div class="mx-10 mt-5 text-white w-5/6 md:w-4/10">
-        <h1 class="text-3xl mb-5">{{ job.name }}</h1>
+      <img :src="job.img" alt="Job Image" class="w-6/8 md:w-3/10 rounded-lg" />
+      <div class="mx-10 mt-5 text-white w-6/8 md:w-4/10">
+        <h1 class="text-3xl font-bold mb-5">{{ job.name }}</h1>
         <p class="text-lg">{{ job.desciptionLong }}</p>
       </div>
     </div>
-    <div class="text-white text-center mt-20 text-3xl mb-30">
-      <p>Siia tuleb info varsti juurde...</p>
-    </div>
+    
+    <component :is="jobComponent" v-if="jobComponent" />
+    
   </template>
   
-  <script setup>
+  <script setup lang="ts">
   import { ref } from 'vue';
   import { useRoute } from 'vue-router';
   import { jobs } from '../models/Portfolio';
@@ -19,6 +19,9 @@
   const route = useRoute();
   const jobId = route.params.jobId; 
   
+  const jobComponent = ref(null);
+  import(`../components/Job${jobId}.vue`).then(module => jobComponent.value = module.default);
+
   const job = ref(jobs.find(job => job.id === Number(jobId)));
   </script>
   
