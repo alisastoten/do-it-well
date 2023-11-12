1ee415e3d7607f3e9ec4da7f84110f10dc1e50ef
alisastoten <alisastoten7@gmail.com>
2023-11-12 16:46:32 +0200

    Hantel profolio images added

---
+++ src/components/Job1.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="w-6/8 mx-auto text-white mt-10 flex flex-col justify-center">
+    <div class="flex flex-col ">
+      <h2 class="text-2xl sm:text-3xl font-semibold mt-4">Eesmärk</h2>
+      <p class="mt-4 leading-relaxed">Eesmärk oli suurendada kasutatud jõusaali- ja kardioseadmete müüki Eestis ja Soomes - see meil ka õnnestus!</p>
+      <h2 class="text-2xl sm:text-3xl font-semibold mt-10 mb-5">Reklaamid</h2>
+      <p class="mb-5">Karusell reklaam</p>
+      <div class="grid grid-cols-1 md:grid-cols-4 gap-2 justify-items-center">
+        <img src="../assets/tehtud-tood/hantel/reklaam.jpg" alt="" class="rounded-xl">
+        <img src="../assets/tehtud-tood/hantel/reklaam2.jpg" alt="" class="rounded-xl">
+        <img src="../assets/tehtud-tood/hantel/reklaam3.jpg" alt="" class="rounded-xl">
+      </div>
+    </div>
+  </div>
+</template>
