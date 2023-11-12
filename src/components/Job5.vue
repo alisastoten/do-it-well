e5eb5afc15c9854f402489a8c3e9a16c4a5ad8c4
alisastoten <alisastoten7@gmail.com>
2023-11-12 17:05:13 +0200

    erplycommerce portfolio pics added

---
+++ src/components/Job5.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="w-6/8 mx-auto text-white mt-10 flex flex-col justify-center">
+    <div class="flex flex-col">
+      <h2 class="text-2xl sm:text-3xl font-semibold mt-4">Eesmärk</h2>
+      <p class="mt-4 leading-relaxed">Luua veebileht ja sarnase temaatikaga reklaamid.</p>
+      <h2 class="text-2xl sm:text-3xl font-semibold mt-10 mb-5">Reklaamid</h2>
+      <p class="mb-5 text-xl">1. Karusell reklaam</p>
+      <div class="grid grid-cols-1 md:grid-cols-5 gap-2 justify-items-center">
+        <img src="../assets/tehtud-tood/erplycommerce/carousel1-400.jpg" alt="" class="rounded-xl">
+        <img src="../assets/tehtud-tood/erplycommerce/carousel2-400.jpg" alt="" class="rounded-xl">
+        <img src="../assets/tehtud-tood/erplycommerce/carousel3-400.jpg" alt="" class="rounded-xl">
+        <img src="../assets/tehtud-tood/erplycommerce/carousel4-400.jpg" alt="" class="rounded-xl">
+        <img src="../assets/tehtud-tood/erplycommerce/carousel5-400.jpg" alt="" class="rounded-xl">
+      </div>
+      <p class="mb-5 text-xl mt-10">2. Üksikpildi reklaam</p>
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-2 justify-items-center">
+        <div>
+          <p class="mb-5 mt-3">Arvutile - FB ja Instagram feed</p>
+          <img src="../assets/tehtud-tood/erplycommerce/reklaam-desktop-400.jpg" alt="" class="rounded-xl">
+        </div>
+        <div>
+          <p class="mb-5 mt-3">Telefonile - story, reels</p>
+          <img src="../assets/tehtud-tood/erplycommerce/reklaam-story-400.jpg" alt="" class="rounded-xl w-full sm:w-60">
+        </div>
+      </div>
+    </div>
+  </div>  
+</template>
